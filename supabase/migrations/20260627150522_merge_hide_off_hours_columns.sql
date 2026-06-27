9616d703e39f7b410969d0d0912bf280f6bfadca
hamedplay <hamedkhaleghi493@gmail.com>
2026-06-27 18:38:01 +0330

    Deleted 20260627150522_merge_hide_off_hours_columns.sql

--- supabase/migrations/20260627150522_merge_hide_off_hours_columns.sql
+++
@@ -1,7 +0,0 @@
--- Merge old hide_off_hours data into hide_offhours
-UPDATE user_preferences
-SET hide_offhours = hide_off_hours
-WHERE hide_off_hours = true AND hide_offhours = false;
-
--- Remove the duplicate old column
-ALTER TABLE user_preferences DROP COLUMN IF EXISTS hide_off_hours;
