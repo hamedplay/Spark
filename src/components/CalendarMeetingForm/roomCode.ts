a614c0ccc5e2664db7aa9690444d7848983afb4e
hamedplay <hamedkhaleghi493@gmail.com>
2026-07-26 19:37:47 +0330

    Delete src/components/CalendarMeetingForm directory

--- src/components/CalendarMeetingForm/roomCode.ts
+++
@@ -1,5 +0,0 @@
-export function generateRoomCode() {
-  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-  const seg = () => Array.from({ length: 3 }, () => chars[Math.floor(Math.random() * chars.length)]).join('');
-  return `${seg()}-${seg()}-${seg()}`;
-}
