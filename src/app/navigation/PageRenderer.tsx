b953e4b11ac8437a2b607c632f51083ac3b8ea77
hamedplay <hamedkhaleghi493@gmail.com>
2026-07-31 23:53:23 +0330

    Updated PageRenderer.tsx

--- src/app/navigation/PageRenderer.tsx
+++ src/app/navigation/PageRenderer.tsx
@@ -53,7 +53,7 @@ export function renderContent(props: PageRendererProps): React.ReactNode {
     );
   }
   const permKey = PAGE_PERMISSION_KEY[activePage];
-  if (permKey && !checkPermission(permKey, isAdmin, userPermissions, hasAnyTrackableDecisions)) {
+  if (permKey && !checkPermission(permKey, isAdmin, userPermissions)) {
     return <AccessDenied onReturn={() => setActivePage('profile')} />;
   }
 
