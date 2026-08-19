548cbe8143c3a7aea7f31fc171232a48f3c41edb
hamedplay <hamedkhaleghi493@gmail.com>
2026-08-19 17:45:46 +0330

    fix: isolate conference UI from auth theme [skip ci]

--- src/GuestApplication.tsx
+++ src/GuestApplication.tsx
@@ -1,13 +1,10 @@
-import './auth-modern.css';
 import { ThemeProvider } from './context/ThemeContext';
 import { GuestJoinPage } from './components/VideoConference/GuestJoinPage';
 
 export default function GuestApplication({ code }: { code: string }) {
   return (
     <ThemeProvider>
-      <div className="spark-auth-flow min-h-screen">
-        <GuestJoinPage code={code} />
-      </div>
+      <GuestJoinPage code={code} />
     </ThemeProvider>
   );
 }
