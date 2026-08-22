d54d9bdb804018f300500a5df5473ec957e4aa0e
hamedplay <hamedkhaleghi493@gmail.com>
2026-08-22 06:16:49 +0330

    Fix Nginx upstream HTTP version for Supabase Envoy

--- deploy/spark-cli/lib/install-platform-b.sh
+++ deploy/spark-cli/lib/install-platform-b.sh
@@ -151,6 +151,7 @@ server {
 
     location ^~ /functions/v1/ {
         proxy_pass http://127.0.0.1:8000;
+        proxy_http_version 1.1;
         proxy_set_header Host \$host;
         proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -158,24 +159,28 @@ server {
 
     location ^~ /auth/v1/ {
         proxy_pass http://127.0.0.1:8000;
+        proxy_http_version 1.1;
         proxy_set_header Host \$host;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
     location ^~ /rest/v1/ {
         proxy_pass http://127.0.0.1:8000;
+        proxy_http_version 1.1;
         proxy_set_header Host \$host;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
     location ^~ /storage/v1/ {
         proxy_pass http://127.0.0.1:8000;
+        proxy_http_version 1.1;
         proxy_set_header Host \$host;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
     location ^~ /graphql/v1 {
         proxy_pass http://127.0.0.1:8000;
+        proxy_http_version 1.1;
         proxy_set_header Host \$host;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
