060efcbe0784829aac92cf7637c063fa013bd1d8
hamedplay <hamedkhaleghi493@gmail.com>
2026-09-05 10:33:21 +0330

    feat: add standalone conference root

---
+++ src/StandaloneConferenceRoot.tsx
@@ -0,0 +1,5 @@
+import StandaloneConferencePage from './components/VideoConference/StandaloneConferencePage';
+
+export default function StandaloneConferenceRoot() {
+  return <StandaloneConferencePage />;
+}
