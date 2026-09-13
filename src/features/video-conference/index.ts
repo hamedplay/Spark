4c53af38a6c8864b3df3f7d4ebfed6dd24a97250
hamedplay <hamedkhaleghi493@gmail.com>
2026-09-13 21:23:22 +0330

    refactor: expose video conference feature API

---
+++ src/features/video-conference/index.ts
@@ -0,0 +1,4 @@
+export { LiveKitConferenceRoom } from './components/LiveKitConferenceRoom';
+export { ConferenceClientContext, useConferenceClient } from './conferenceClient';
+export type { ConferenceSupabaseClient } from './conferenceClient';
+export type { ConferenceRoomShape } from './types/conference.types';
